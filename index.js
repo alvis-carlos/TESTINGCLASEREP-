5d44ef0887e834ff720df4ebac420865be70c6d5
alvis-carlos <57160160+alvis-carlos@users.noreply.github.com>
2021-09-08 22:46:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = express()
 
 
-app.use(required('./requires/author1'));
-app.use(required('./requires/author2'));
+app.use(require('./requires/author1'));
+app.use(require('./requires/author2'));
 
 app.listen(3000)
