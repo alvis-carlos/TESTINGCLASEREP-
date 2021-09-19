612a1e019afde6b521dcb2c8bcd4312b6ff5a142
alvis-carlos <57160160+alvis-carlos@users.noreply.github.com>
2021-09-19 15:27:30 -0500

    Archivo principal

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
+import {paises,ciudades,datos,concatenar,contarVocales,ConvertirMayusculas,datoVariado,datoVariado2,datoVariad3} from "./moduleOne"
+import {number1,number2,number3,boolean1,boolean2,boolean3} from "./moduleTwo"
 
+
+console.log(number1);
+console.log(number2);
+console.log(number3);
+
+
+console.log(boolean1);
+console.log(boolean2);
+console.log(boolean3);
