c59ed630296e65a7955da930dd01f1e8c6987746
alvis-carlos <57160160+alvis-carlos@users.noreply.github.com>
2021-09-19 21:14:52 -0500

    Archivos librerias faltantes

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import {paises,ciudades,datos,concatenar,contarVocales,ConvertirMayusculas,datoVariado,datoVariado2,datoVariad3} from "./ModuleOne"
+//import {paises,ciudades,datos,concatenar,contarVocales,ConvertirMayusculas,datoVariado,datoVariado2,datoVariad3} from "./ModuleOne"
 import {number1,number2,number3,boolean1,boolean2,boolean3} from "./ModuleTwo"
 
 
@@ -14,15 +14,15 @@ console.log(boolean3);
 
 
 
-console.log(paises);
-console.log(ciudades);
-console.log('Nombres: ' + datos[0]+ ' '+datos[1]+"\nedad: "+datos[2]+ "\nCiudad: "+ datos[3]+"\nsalario: "+datos[4] );
+// console.log(paises);
+// console.log(ciudades);
+// console.log('Nombres: ' + datos[0]+ ' '+datos[1]+"\nedad: "+datos[2]+ "\nCiudad: "+ datos[3]+"\nsalario: "+datos[4] );
 
 
-console.log(concatenar('hola',concatenar('mundo','!!!')));
-console.log(contarVocales('HOLA Mundo'));
-console.log(ConvertirMayusculas('Hola'));
+// console.log(concatenar('hola',concatenar('mundo','!!!')));
+// console.log(contarVocales('HOLA Mundo'));
+// console.log(ConvertirMayusculas('Hola'));
 
-console.log(datoVariado);
-console.log(datoVariado2);
-console.log(datoVariado3);
+// console.log(datoVariado);
+// console.log(datoVariado2);
+// console.log(datoVariado3);
